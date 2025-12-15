996d8cb0159395527448acf15bc720d4a1b32709
Григорий Слюсаренко <slyusarenko.grigory@mail.ru>
2025-12-15 13:09:09 +0300

    Добавлены функции умножения и вычитания

--- calculator.py
+++ calculator.py
@@ -1,7 +1,13 @@
-def add(a, b): 
+def add(a, b):
 	return a + b
 
-def divide(a, b): 
-	if b == 0: 
-		raise ValueError("Деление на ноль невозможно") 
+def divide(a, b):
+	if b == 0:
+		raise ValueError("Деление на ноль невозможно")
 	return a / b
+
+def multiply(a, b):
+	return a * b
+
+def subtract(a, b):
+return a - b
